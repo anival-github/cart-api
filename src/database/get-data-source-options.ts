ea20385044f20e6545e99be3608dbfb706b55546
anival-github <69170871+anival-github@users.noreply.github.com>
2023-07-22 13:25:03 +0200

    task-9-new (#4)
    Co-authored-by: Valiantsin Anikin <valiantsin.anikin@bayer.com>

--- src/database/get-data-source-options.ts
+++ src/database/get-data-source-options.ts
@@ -3,9 +3,7 @@ import { DbConfigProvider } from './database-config.provider';
 import { CartEntity } from 'src/cart/models/cart.entity';
 import { CartItemEntity } from 'src/cart/models/cart-item.entity';
 import { OrderEntity } from 'src/order/models/order.entity';
-import { Orders1689181237819 } from './migrations/1689181237819-orders';
-import { CartItems1689181221066 } from './migrations/1689181221066-cartItems';
-import { Carts1689181187203 } from './migrations/1689181187203-carts';
+import { Carts1689181187203 } from './migrations/1689181187203-all';
 
 export default function getDataSourceOptions(
   dbConfigProvider: DbConfigProvider,
@@ -22,7 +20,12 @@ export default function getDataSourceOptions(
     entities: [CartEntity, CartItemEntity, OrderEntity],
     password,
     ssl: true,
-    migrations: [Orders1689181237819, CartItems1689181221066, Carts1689181187203],
+    migrations: [Carts1689181187203],
     migrationsRun: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false
+      },
+    }
   };
 }
